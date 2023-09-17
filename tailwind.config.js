d4bc1fc0525efb7d7a9675e330d7219c7f1925eb
“TobiA34” <tobiadegoroye49@gmail.com>
2023-09-17 22:22:30 +0100

    added new code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-    './index.html'
-  ],
+  content: ["./dist/**/*.{html,js}"],
     theme: {
     extend: {},
   },
